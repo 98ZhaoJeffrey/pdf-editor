2540468f33da912395d82700751884d6ea3ae001
Dani3lx <1danielxu@gmail.com>
2023-06-03 00:33:26 -0400

    fixed the left margin

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -4,7 +4,7 @@ function Landing() {
     return (
         <>
             <div className='flex w-full h-screen justify-between' >
-                <div className='m-5 sm:mt-12 sm:ml-12 md:ml-20  xl:mt-20 xl:ml-72 max-w-lg'>
+                <div className='m-5 sm:mt-12 sm:ml-12 md:ml-20  xl:mt-20 xl:ml-32 max-w-lg'>
                     <h2 className='text-2xl sm:text-4xl lg:text-title font-bold text-secondary'>Untitled</h2>
                     <h1 className='text-5xl sm:text-6xl lg:text-heading mt-28 lg:mt-32 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary-200 to-secondary'>The fastest and easiest way to edit PDFs </h1>
                     <h3 className='lg:text-description max-w-xs mt-5 mb-10'>A simple and robust PDF editor all in your browser</h3>
