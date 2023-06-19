fc45139b3c21d9b3e15847881648910fe267dce8
Dani3lx <1danielxu@gmail.com>
2023-06-18 21:22:33 -0400

    Separated the client component in FolderDisplay to the leave of the component tree to follow next js best practice

--- src/app/components/FolderDisplay.tsx
+++ src/app/components/FolderDisplay.tsx
@@ -1,13 +1,9 @@
-'use client'
-import React, { useContext } from 'react'
+import React from 'react'
 import FolderGridCard from './FolderGridCard'
 import PDFGridCard from './PDFGridCard'
-
-import { BsGrid3X3Gap, BsListUl } from "react-icons/bs";
 import FolderListCard from './FolderListCard';
 import PDFListCard from './PDFListCard';
-
-import { ViewPreferenceContext } from '../contexts/ViewPreferenceContext';
+import ClientFolderRender from './ClientFolderRender';
 
 const GridView: React.FC = () => {
     return (
@@ -44,23 +40,8 @@ interface FolderDisplayProps {
 }
 
 const FolderDisplay: React.FC<FolderDisplayProps> = ({ category }) => {
-    const { gridView, toggleView } = useContext(ViewPreferenceContext);
     return (
-        <div className='pt-5 pl-14'>
-            <div className='flex justify-between'>
-                <h1 className='text-title'>{category}</h1>
-                <button
-                    onClick={toggleView}
-                    className='rounded-full mr-20 mt-1 hover:bg-slate-200 p-5'
-                    title={gridView ? 'Grid view' : 'List view'}
-                >
-                    {gridView ? <BsGrid3X3Gap size={25} /> : <BsListUl size={25} />}
-                </button>
-            </div>
-            <hr className='mb-5 mt-8 mr-5' />
-            {/* Displays all of the pdf files */}
-            {gridView ? <GridView /> : <ListView />}
-        </div>
+        <ClientFolderRender GridView={<GridView />} ListView={<ListView />} category={category} />
     )
 }
 
