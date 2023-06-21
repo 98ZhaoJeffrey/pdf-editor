8dae8ff8902d1dac85a0343a2fcc9fd7543a976b
Dani3lx <1danielxu@gmail.com>
2023-06-20 20:01:07 -0400

    Draft implementation of quering the folders

--- src/app/api/folders/route.ts
+++ src/app/api/folders/route.ts
@@ -4,7 +4,6 @@ import { auth } from '@clerk/nextjs';
 
 export async function POST(request: Request) {
     const res = await request.json()
-    console.log(res)
     const { userId } = auth();
     await prisma.folder.create({
         data: {
--- src/app/dashboard/folders/[folderID]/page.tsx
+++ src/app/dashboard/folders/[folderID]/page.tsx
@@ -1,8 +1,54 @@
 import FolderDisplay from '@/app/components/FolderDisplay'
-import React from 'react'
+import { prisma } from '@/app/lib/prisma';
+import React from "react";
+import { auth } from '@clerk/nextjs';
 
-export default function Page({ params }: { params: { folderID: string } }) {
-    const folderName = "Folder Name"
+export default async function Page({ params }: { params: { folderID: string } }) {
     const folderID = params.folderID
+    const { userId } = auth();
+    let currFolder;
+    try {
+        currFolder = await prisma.folder.findUnique({
+            where: {
+                id: folderID
+            }
+        })
+    } catch (e) {
+        return (
+            <>
+                <h1>Invalid folder link</h1>
+            </>)
+    }
+
+    if (currFolder === null) {
+        return (
+            <>
+                <h1>This folder does not exist</h1>
+            </>)
+    }
+    if (currFolder.ownerId !== userId) {
+        return (
+            <>
+                <h1>Permission denied</h1>
+            </>)
+    }
+
+    const folderName = currFolder.name;
+
+    const folders = await prisma.folder.findMany({
+        where: {
+            folderParentFolderId: {
+                equals: folderID,
+            },
+            AND: {
+                ownerId: {
+                    equals: String(userId)
+                }
+            }
+        }
+    })
+
+    console.log(folders);
+
     return (<FolderDisplay category={folderName} />)
 }
