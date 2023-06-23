dc2e97676de68eede8539d0a5eb7e77d7c2846ef
Dani3lx <1danielxu@gmail.com>
2023-06-22 20:49:04 -0400

    Added input field for uploading new pdf file

--- src/app/components/AddFileButton.tsx
+++ src/app/components/AddFileButton.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent } from 'react'
 import { MdAdd } from 'react-icons/md'
 
 function AddFileButton() {
+    const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        // Perform upload logic with the selected file here
+        console.log('Selected file:', file?.name);
+    };
+
+    const handleClick = () => {
+        const input = document.createElement('input');
+        input.name = 'userfile';
+        input.type = 'file';
+        input.accept = 'application/pdf';
+        input.addEventListener('change', (event) =>
+            handleFileUpload(event as unknown as ChangeEvent<HTMLInputElement>)
+        );
+        input.click();
+    };
+
     return (
-        <button className='text-white w-full text-center p-5 rounded-xl hover:bg-emerald-600 hover:text-white font-medium'>
+        <button className='text-white w-full text-center p-5 rounded-xl hover:bg-emerald-600 hover:text-white font-medium'
+            onClick={handleClick}>
             <div className='flex items-center justify-start gap-3'>
                 <MdAdd size={20} /> New File
             </div>
