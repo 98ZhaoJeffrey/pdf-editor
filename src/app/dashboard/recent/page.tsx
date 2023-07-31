5f984d6f9f34feb54245851125b5804f2219320f
Dani3lx <1danielxu@gmail.com>
2023-07-31 02:48:27 -0400

    Made Recent tab work

--- src/app/dashboard/recent/page.tsx
+++ src/app/dashboard/recent/page.tsx
@@ -1,9 +1,31 @@
 import FolderDisplay from '@/app/components/Folder/FolderDisplay';
 import React from 'react'
+import { prisma } from '@/app/lib/prisma';
+import { auth } from '@clerk/nextjs';
 
-const Page: React.FC = () => {
+
+const Page: React.FC = async () => {
+    const { userId } = auth();
+
+    const files = await prisma.file.findMany({
+        where: {
+            ownerId: {
+                equals: String(userId)
+            }
+        },
+        select: {
+            name: true,
+            lastUpdated: true,
+            size: true,
+            id: true
+        },
+        orderBy: {
+            lastUpdated: 'desc'
+        }
+    })
+
     return (
-        <FolderDisplay category='Recent' folders={[]} files={[]}/>
+        <FolderDisplay category='Recent' folders={[]} files={files} />
     );
 };
 
