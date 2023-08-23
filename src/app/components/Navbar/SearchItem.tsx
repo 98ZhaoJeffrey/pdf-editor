3c2214c3a190ddfb7bb6e4f894484b9a847100ad
Dani3lx <1danielxu@gmail.com>
2023-08-23 15:33:47 -0400

    Connected the pdf cards in dashboard to the editor

--- src/app/components/Navbar/SearchItem.tsx
+++ src/app/components/Navbar/SearchItem.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link';
 import React from 'react'
 import { AiFillFolder, AiFillFilePdf } from 'react-icons/ai';
-import { useRouter } from 'next/navigation'
 
 interface SearchItemProps {
     isFolder: boolean;
@@ -11,9 +10,9 @@ interface SearchItemProps {
 }
 
 function SearchItem({ isFolder, name, id }: SearchItemProps) {
-    const router = useRouter()
+    const destinationLink = isFolder ? `/dashboard/folders/${id}` : `/editor/${id}`
     return (
-        <Link className={`w-full flex justify-between px-5 py-2 bg-white z-10 relative hover:bg-slate-200`} href={`/dashboard/folders/${id}`}>
+        <Link className={`w-full flex justify-between px-5 py-2 bg-white z-10 relative hover:bg-slate-200`} href={destinationLink}>
             <div className='flex gap-5'>
                 {isFolder ? <AiFillFolder size={25} /> : <AiFillFilePdf size={25} color='red'/>}
                 <h1 className='w-96 truncate'>{name}</h1>
